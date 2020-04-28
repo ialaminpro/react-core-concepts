40014e375433531a83efc9ed0060867fb60fe056
Al-Amin <ialamin.pro@gmail.com>
2020-04-29 01:44:39 +0600

    Pass object to components and access object

--- src/App.js
+++ src/App.js
@@ -29,10 +29,11 @@ function Product(props){
     width:'200px',
     float:'left'
   }
+  const {name,price} = props.product;
   return (
     <div style={productStyle}>
-      <h3>{props.product.name}</h3>
-      <h5>{props.product.price}</h5>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
       <button>Buy now</button>
     </div>
   )
