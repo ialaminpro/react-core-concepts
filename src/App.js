172a3caa7d65d4e480c024e31f61e72fe701b663
Al-Amin <ialamin.pro@gmail.com>
2020-04-29 01:40:31 +0600

    Pass object to components and access object

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import './App.css';
 
 function App() {
   const nayoks = ['Anwar','Jafor Iqbal','Alamgir', 'Salman']
+  const products = [
+    {name: 'Photoshop', price:'$90.99'},
+    {name: 'Illustrator', price:'$60.99'},
+    {name: 'PDF Reader', price:'$6.99'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <p>My First React Paragraph</p>
+        <p>I am a React Person</p>
+        <Product name={products[0].name} price={products[0].price}></Product>
         <Person name={nayoks[0]}></Person>
         <Person name={nayoks[1]}></Person>
       </header>
     </div>
   );
+}
+function Product(props){
+  const productStyle={
+    border:'1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+  return (
+    <div style={productStyle}>
+      <h3>{props.name}</h3>
+      <h5>{props.price}</h5>
+      <button>Buy now</button>
+    </div>
+  )
 }
 function Person(props){
   const personStyle={
