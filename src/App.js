3b3c630d29d088e4ea1812fd83edb3e4b385daf5
Al-Amin <ialamin.pro@gmail.com>
2020-04-29 02:27:41 +0600

    Component state hook and set state method

--- src/App.js
+++ src/App.js
@@ -35,10 +35,11 @@ function App() {
 }
 
 function Counter(){
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
   return(
     <div>
       <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count-1)}>Decrease</button>
       <button onClick={() => setCount(count+1)}>Increase</button>
     </div>
   )
